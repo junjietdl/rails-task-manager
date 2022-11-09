750e0a1115d66e97d56ec19601085d582f6d806d
Jun Jie Teo <junjietdl@gmail.com>
2022-11-09 17:20:07 +0800

    added task app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'tasks#index'
+
+  # # Read (all) #INDEX
+  # get 'tasks', to: 'tasks#index'
+  # # Create Step 1, GET the form
+  # get 'tasks/new', to: 'tasks#new'
+  # # Create - Step 2, POST the form
+  # post 'tasks', to: 'tasks#create'
+  # # Update - Step 1, GET the form
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # # Read (one) #SHOW
+  # get 'tasks/:id', to: 'tasks#show'
+
+  resources :tasks
+
 end
